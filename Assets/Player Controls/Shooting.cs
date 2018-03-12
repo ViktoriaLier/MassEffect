fe440a36c76df172bee8349a83a3512e92a4e801
Viktoria <v.lierhaus@gmx.de>
2018-03-12 14:42:05 +0100

    idk

--- Assets/Player Controls/Shooting.cs
+++ Assets/Player Controls/Shooting.cs
@@ -1,32 +1,22 @@
 ﻿using UnityEngine;
-using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour
+
+public class Shooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 
     void Update()
     {
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetMouseButtonDown(0))
         {
             Fire();
         }
     }
 
 
-    void Fire()
+    public void Fire()
     {
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(
@@ -35,14 +25,11 @@ public class PlayerController : NetworkBehaviour
             bulletSpawn.rotation);
 
         // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 30;
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
     }
 
-    public override void OnStartLocalPlayer()
-    {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
-    }
+   
 }
--- Assets/Player Controls/Walking.cs
+++ Assets/Player Controls/Walking.cs
@@ -2,15 +2,70 @@
 
 public class Walking : MonoBehaviour {
 
+    public float horizontalSpeed = 1f;
+    public float verticalSpeed = 1f;
+
+    public Animator anim;
+    public Rigidbody rbody;
+
+    private float inputH;
+    private float inputV;
+    private bool run;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        run = false;
+    }
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
+        if (Input.GetKeyDown("w"))
+        {
+            anim.Play("WALK00_F", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            anim.Play("WALK00_B", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            anim.Play("WALK00_L", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            anim.Play("WALK00_R", -1, 0f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+
+        float moveX = inputH * 20f * Time.deltaTime;
+        float moveZ = inputV * 50f * Time.deltaTime;
+
+        rbody.velocity = new Vector3(moveX, 0f, moveZ);
+
     }
-
-
 }
